ff80a4c5e3c8114c621459498142a57e0acc307f
Jay A. Keraliya <jaykeraliya0@gmail.com>
2022-08-25 17:00:50 +0530

    commiting rinkal-27;s changes in barchart.js file

--- ChartJS-in-React/src/components/BarChart.js
+++ ChartJS-in-React/src/components/BarChart.js
@@ -1,67 +1,79 @@
-import React from 'react'
-import { Pie, defaults } from 'react-chartjs-2'
+import React from "react";
+import { Bar, defaults } from "react-chartjs-2";
 
-defaults.global.tooltips.enabled = false
-defaults.global.legend.position = 'bottom'
+defaults.global.tooltips.enabled = false;
+defaults.global.legend.position = "bottom";
 
 const BarChart = () => {
-  return (
-    <div>
-      <Pie
-        data={{
-          labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-          datasets: [
-            {
-              label: '# of votes',
-              data: [12, 19, 3, 5, 2, 3],
-              backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)',
-              ],
-              borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)',
-              ],
-              borderWidth: 1,
-            },
-            // {
-            //   label: 'Quantity',
-            //   data: [47, 52, 67, 58, 9, 50],
-            //   backgroundColor: 'orange',
-            //   borderColor: 'red',
-            // },
-          ],
-        }}
-        height={400}
-        width={600}
-        options={{
-          maintainAspectRatio: false,
-          scales: {
-            yAxes: [
-              {
-                ticks: {
-                  beginAtZero: true,
-                },
-              },
-            ],
-          },
-          legend: {
-            labels: {
-              fontSize: 25,
-            },
-          },
-        }}
-      />
-    </div>
-  )
-}
+    return (
+        <div>
+            <Bar
+                data={{
+                    labels: [
+                        "N1",
+                        "N2",
+                        "N3",
+                        "N4",
+                        "N5",
+                        "N6",
+                        "N7",
+                        "N8",
+                        "N9",
+                        "N10",
+                        "N11",
+                        "N12",
+                    ],
+                    datasets: [
+                        {
+                            label: "NQM Survey",
+                            data: [
+                                100, 100, 100, 100, 50, 100, 100, 100, 50, 100,
+                                100, 50, 100,
+                            ],
+                            backgroundColor: "rgba(255,99,132,0.2)",
+                            borderColor: "rgba(255, 99, 132, 1)",
+                            borderWidth: 1,
+                        },
+                        {
+                            label: "SQM Survey",
+                            data: [
+                                100, 100, 50, 100, 100, 100, 100, 50, 100, 50,
+                                100, 100, 100,
+                            ],
+                            backgroundColor: "rgba(75, 192, 192, 0.2)",
+                            borderColor: "rgba(75, 192, 192, 1)",
+                            borderWidth: 1,
+                        },
+                    ],
+                }}
+                height={400}
+                width={600}
+                options={{
+                    title: {
+                        display: true,
+                        text: "ANALYSIS OF NQM AND NQM SURVEY",
+                        fontSize: 35,
+                        fontFamily: "sans-serif",
+                    },
+                    maintainAspectRatio: false,
+                    scales: {
+                        yAxes: [
+                            {
+                                ticks: {
+                                    beginAtZero: true,
+                                },
+                            },
+                        ],
+                    },
+                    legend: {
+                        labels: {
+                            fontSize: 20,
+                        },
+                    },
+                }}
+            />
+        </div>
+    );
+};
 
-export default BarChart
+export default BarChart;
