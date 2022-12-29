e999862c9fa3b473404da56e177195647cbd8fc0
Jay Keraliya <jaykeraliya0@gmail.com>
2022-12-29 21:02:38 +0530

    applied fixes

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -3,16 +3,16 @@ import Head from "next/head";
 import Navbar from "./components/Navbar";
 
 const Home: NextPage = () => {
-    return (
-        <div>
-            <Head>
-                <title>Home</title>
-                <link rel="icon" href="/favicon.ico" />
-            </Head>
-            <Navbar current="/" />
-            <img src="/hm.jpg" alt="logo" />
-        </div>
-    );
+  return (
+    <div>
+      <Head>
+        <title>Home</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Navbar current="/" />
+      <img src="/hm.jpg" alt="logo" />
+    </div>
+  );
 };
 
 export default Home;
